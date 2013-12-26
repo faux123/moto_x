cf824e23d89d40872ed83b0ff086a8b0a2d7f8df
Paul Reioux <reioux@gmail.com>
2013-12-25 23:09:12 -0800

    sweep2wake/doubletap2wake: adapt for falcon_umts (aka Moto G)
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	drivers/input/touchscreen/synaptics_dsx_i2c.c

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -37,6 +37,11 @@
 #include <linux/input/mt.h>
 #endif
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#include <linux/input/sweep2wake.h>
+#include <linux/input/doubletap2wake.h>
+#endif
+
 #define DRIVER_NAME "synaptics_dsx_i2c"
 #define INPUT_PHYS_NAME "synaptics_dsx_i2c/input0"
 #define TYPE_B_PROTOCOL
@@ -3641,6 +3646,12 @@ static int synaptics_rmi4_suspend(struct device *dev)
 	const struct synaptics_dsx_platform_data *platform_data =
 			rmi4_data->board;
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+	if ((s2w_switch > 0) || (dt2w_switch > 0)) {
+		pr_info("suspend avoided!\n");
+		return 0;
+	} else {
+#endif
 	synaptics_dsx_sensor_state(rmi4_data, STATE_SUSPEND);
 	if (!rmi4_data->touch_stopped) {
 		if (platform_data->regulator_en)
@@ -3650,6 +3661,9 @@ static int synaptics_rmi4_suspend(struct device *dev)
 
 		rmi4_data->touch_stopped = true;
 	}
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+	}
+#endif
 
 	return 0;
 }
