bec3fb0477867b5d06941bfc6e9a3cfdab7388ef
Paul Reioux <reioux@gmail.com>
2014-02-10 23:23:19 -0800

    CPU OVERCLOCKING: tweak for Moto X
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/msm_dcvs.c
+++ arch/arm/mach-msm/msm_dcvs.c
@@ -146,7 +146,12 @@ static struct dcvs_core core_list[CORES_MAX];
 
 static struct kobject *cores_kobj;
 
+#ifndef CONFIG_CPU_OVERCLOCKING
 #define DCVS_MAX_NUM_FREQS 15
+#else
+#define DCVS_MAX_NUM_FREQS 42
+#endif
+
 static struct msm_dcvs_freq_entry cpu_freq_tbl[DCVS_MAX_NUM_FREQS];
 static unsigned num_cpu_freqs;
 static struct msm_dcvs_platform_data *dcvs_pdata;
